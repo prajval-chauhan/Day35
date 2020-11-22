67be08f7c8641ae1cd1d402c276d90b3286fc625
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-11-22 10:05:24 +0530

    Created a contact class with vairous properties

--- AddressBook.js
+++ AddressBook.js
@@ -6,7 +6,6 @@ class contacts
     //properties of the contacts
     firstName;
     lastName;
-    address;
     city;
     state;
     zip;
@@ -17,11 +16,10 @@ class contacts
     {
         this.firstName = params[0];
         this.lastName = params[1];
-        this.address = params[2];
-        this.city = params[3];
-        this.state = params[4];
-        this.zip = params[5];
-        this.phoneNumber = params[6];
-        this.email = params[7];
+        this.city = params[2];
+        this.state = params[3];
+        this.zip = params[4];
+        this.phoneNumber = params[5];
+        this.email = params[6];
     }
 }
